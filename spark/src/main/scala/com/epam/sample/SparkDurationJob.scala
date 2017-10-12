032408802ff70301f72dee37fe4f68e00c4f2289
Aleksandr Chermenin <aleksandr_chermenin@epam.com>
2017-10-12 13:23:17 +0300

    Updated Spark base job

--- spark/src/main/scala/com/epam/sample/SparkDurationJob.scala
+++ spark/src/main/scala/com/epam/sample/SparkDurationJob.scala
@@ -8,14 +8,13 @@ class SparkDurationJob extends SparkBaseJob("local[2]", "Spark Duration Job") {
   override def process(stream: DStream[DeviceMessage]): DStream[IndexMessage] = {
     // Put your code here...
 
-    null
+
   }
 }
 
 object SparkDurationJob {
 
   def main(args: Array[String]): Unit = {
-    val l = List("1", "2", "3") ++ List("45")
-    print(l)
+    new SparkDurationJob().execute()
   }
 }
